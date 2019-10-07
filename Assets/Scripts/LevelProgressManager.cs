52a18b432483ae1cb952cc7c041af53fbec97e59
TobiCode <tobias.w.weber@gmail.com>
2019-10-07 19:56:24 +0200

    Add a new Script that should Save and Load the Level Progress of the Player.

---
+++ Assets/Scripts/LevelProgressManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgressManager : MonoBehaviour
+{
+    public static int levelProgress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelProgress = LoadProgress();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    static void SaveLevelProgress(int progress)
+    {
+        if (levelProgress < progress)
+        {
+            PlayerPrefs.SetInt("level_Progress", progress);
+            PlayerPrefs.Save();
+        }
+    }
+
+    static int LoadProgress()
+    {
+        int progress = PlayerPrefs.GetInt("level_Progress", 1);
+        if (progress > levelProgress)
+        {
+            levelProgress = progress;
+        }
+        return progress;
+    }
+}
