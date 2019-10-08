61941d40bff3b2b50ca1cb92338b03e713fa6ad1
TobiCode <tobias.w.weber@gmail.com>
2019-10-08 18:18:42 +0200

    Finish GameOver Screen.

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,17 +7,24 @@ using UnityEngine.UI;
 public class GameManagerScript : MonoBehaviour
 {
 
-    public static int widthOfMaze=6;
-    public static int heightOfMaze=6;
+    public static int widthOfMaze = 6;
+    public static int heightOfMaze = 6;
     public GameObject player;
     private CharacterControllerTobi playerScript;
     private GameObject enemy;
     public GameObject mazeCam;
+    public GameObject mainCam;
+    public Vector3 cameraPosLookingBack;
+    public Vector3 cameraRotLookingBack;
     public GameObject PathLights;
     public GameObject gameUIObject;
     public GameObject gameOverUIObject;
+    public Image darkenImage;
+    public Image scratch1;
+    public Image scratch2;
     public Text infoText;
     private bool play;
+    private bool scratchesShown;
 
     public MazeGenerator_AfterBeatuifyAndPerformanceUpgrade mazeGenScript;
     public CameraMazeScript cameraMazeScript;
@@ -51,11 +58,20 @@ public class GameManagerScript : MonoBehaviour
             enemy.SetActive(true);
             StartCoroutine(showInfoText());
         }
-        if(playerScript.Live < 1)
+        if (playerScript.Live < 1)
         {
-            //Stop Game and show Game Over Canvas
-            gameOverUIObject.SetActive(true);
-            gameUIObject.SetActive(false);
+            playerScript.enabled = false;
+            mainCam.transform.localPosition = cameraPosLookingBack;
+            mainCam.transform.localRotation = Quaternion.Euler(cameraRotLookingBack);
+            //show Scratches
+            StartCoroutine(ShowScratches());
+            if (scratchesShown)
+            {
+                //Stop Game and show Game Over Canvas
+                gameOverUIObject.SetActive(true);
+                gameUIObject.SetActive(false);
+                StartCoroutine(FadeImage(false, darkenImage, 240f, 0.5f));
+            }
         }
     }
 
@@ -74,5 +90,43 @@ public class GameManagerScript : MonoBehaviour
         infoText.enabled = true;
         yield return new WaitForSeconds(3);
         infoText.enabled = false;
+    }
+
+    IEnumerator FadeImage(bool fadeAway, Image img, float alpha, float overTime)
+    {
+        float startTime = Time.time;
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            float beginningAlpha = img.color.a;
+            // loop over 1 second backwards
+            while (Time.time < startTime + overTime)
+            {
+                float fraction = (Time.time - startTime) / overTime * beginningAlpha;
+                img.color = new Color(0, 0, 0, beginningAlpha - fraction);
+                yield return null;
+            }
+            scratchesShown = true;
+        }
+        // fade from transparent to opaque
+        else
+        {
+            while (Time.time < startTime + overTime)
+            {
+                float fraction = (Time.time - startTime) / overTime * alpha;
+                img.color = new Color(0, 0, 0, fraction / 255f);
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator ShowScratches()
+    {
+        //FadeIN
+        yield return  StartCoroutine(FadeImage(false, scratch1, 255f, 0.1f));
+        yield return  StartCoroutine(FadeImage(false, scratch2, 255f, 0.1f));
+        //FadeOUT
+        StartCoroutine(FadeImage(true, scratch1, 0f, 0.2f));
+        StartCoroutine(FadeImage(true, scratch2, 0f, 0.2f));
     }
 }
